577ee349860cacfd74e5f8a9268ff48f420a5a3a
Jacob Barber <jacob.barber@disneystreaming.com>
2019-11-12 13:21:01 -0500

    Change hardcoded quiet=false to use supplied quietness

--- bigolchungus.cpp
+++ bigolchungus.cpp
@@ -122,7 +122,7 @@ int main(int argc, const char * const * argv) {
     fread(&start_nonce, 1, 8, urandom);
     fclose(urandom);
 
-    opencl_backend backend(nonce_step_size, true, deviceOverride, platformOverride);
+    opencl_backend backend(nonce_step_size, quiet, deviceOverride, platformOverride);
 
     backend.start_search(
         global_size, local_size, workset_size,
